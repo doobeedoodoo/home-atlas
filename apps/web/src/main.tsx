b66eebd3d62e2b51bed307a0b27e056046f23488
Mac Sta Maria <mac.stamaria@gmail.com>
2026-03-25 16:47:01 +1000

    feat: add vercel analytics

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,4 +1,5 @@
 import { createRoot } from 'react-dom/client';
+import { Analytics } from '@vercel/analytics/react';
 import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ThemeProvider } from '@mui/material/styles';
@@ -24,6 +25,7 @@ createRoot(document.getElementById('root')!).render(
         <ThemeProvider theme={theme}>
           <CssBaseline />
           <App />
+          <Analytics />
         </ThemeProvider>
       </QueryClientProvider>
     </BrowserRouter>
